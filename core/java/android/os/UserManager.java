d414188c7bcb805359abced884b109d6e6026068
Chuck439 <firmankediri123@gmail.com>
2022-03-19 04:53:11 +0000

    Revert "gmscompat: fix push notifications in secondary user profiles"
    This reverts commit 2f07969b2f5659d6918df99790d929dd489ddba3.

--- core/java/android/os/UserManager.java
+++ core/java/android/os/UserManager.java
@@ -2527,9 +2527,6 @@ public class UserManager {
     @RequiresPermission(anyOf = {Manifest.permission.MANAGE_USERS,
             Manifest.permission.INTERACT_ACROSS_USERS}, conditional = true)
     public boolean isUserUnlocked(@UserIdInt int userId) {
-        if (GmsCompat.isEnabled()) {
-            return mIsUserUnlockedCache.query(mUserId);
-        }
         return mIsUserUnlockedCache.query(userId);
     }
 
