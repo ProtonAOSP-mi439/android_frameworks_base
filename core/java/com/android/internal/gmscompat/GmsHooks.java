0b02723cefb8983a6b9613c6821ab4dfaaefd9da
Chuck439 <firmankediri123@gmail.com>
2022-03-19 04:55:30 +0000

    Revert "gmscompat: simplify initialization"
    This reverts commit 6474ed8b3a2d8effa7741ecaae9ca4afed1ae9dd.

--- core/java/com/android/internal/gmscompat/GmsHooks.java
+++ core/java/com/android/internal/gmscompat/GmsHooks.java
@@ -152,7 +152,7 @@ public final class GmsHooks {
 
     // Instrumentation#newApplication(ClassLoader, String, Context)
     public static void initApplicationBeforeOnCreate(Application app) {
-        GmsCompat.maybeEnable(app);
+        GmsCompat.initChangeEnableStates(app);
 
         if (GmsCompat.isEnabled()) {
             String processName = Application.getProcessName();
